17468ce1b6c8fb2912946e5a5614dddbd5c1815a
abdulrazaq_pro <abdulrazmail@gmail.com>
2024-11-19 00:16:05 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html' // Targets the index.html file at the root of your project directory
+  ],
+  darkMode: 'class', // Ensure dark mode is controlled by the 'dark' class
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
